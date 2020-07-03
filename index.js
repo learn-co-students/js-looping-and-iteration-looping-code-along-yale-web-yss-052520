1382fa1dc85dc3efd78c97218026892e119ca339
nicolewang2000 <nicole.wang@yale.edu>
2020-07-03 19:05:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+    let cards = []
+    for (let i = 0; i < array.length; i++){
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+writeCard(array, event)
+
+function countDown(num){
+    while (num != 0) {
+        console.log(num)
+        num -= 1
+    }
+    console.log(num)
+}
