1a29affdd31eceedece261b06ce838d9e89de082
ZIming Mao <ziming.mao@yale.edu>
2020-07-04 10:22:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let arr = []
+    for (let i = 0; i < names.length; i ++ ){
+        arr.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+    }
+    return arr
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+}
