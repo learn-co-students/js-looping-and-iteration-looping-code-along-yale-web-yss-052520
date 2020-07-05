c8ace7e92ea8758855ef1293bf46c8497492ee38
Eric Ou <eric.ou@yale.edu>
2020-07-04 20:05:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+function writeCards(arr, gift_type) {
+    for (let index = 0; index < arr.length; index++){
+        arr[index] = `Thank you, ${arr[index]}, for the wonderful ${gift_type} gift!`;
+    }
+    return arr;
+}
+
+function countDown(start){
+    while (start >= 0) {
+        console.log(start);
+        start -= 1
+    }
+}
