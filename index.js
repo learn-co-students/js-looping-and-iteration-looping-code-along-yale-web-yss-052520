97891fcaa1fd30ec4b537709615134b1554c15da
s-dney <littlebirdsword@gmail.com>
2020-07-06 14:50:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, word) {
+  let cards = []
+  for (let i=0; i<array.length; i++) {
+    cards.push(`Thank you, ${array[i]}, for the wonderful ${word} gift!`)
+  }
+  return cards
+}
+
+// writeCards(["Anna", "Vidhi"], "birthday")
+
+function countDown(num) {
+  for (let i = 0; i<=num; i++) {
+    console.log(num-i)
+  }
+}
+
+countDown(11)
