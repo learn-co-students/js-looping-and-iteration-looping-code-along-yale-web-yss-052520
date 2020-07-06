57d38b7a9a5af55f82f3f5b0d29a2bfbb8267804
dlee3427 <david.lee.sl2634@yale.edu>
2020-07-06 16:11:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event) {
+    let arrayNew = []
+    for (let i = 0; i < names.length; i++) {
+      arrayNew.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return arrayNew
+  }
+
+function countDown(number) {
+    while(number > 0) {
+        console.log(number);
+        number -= 1; 
+    }
+    console.log(number)
+}
