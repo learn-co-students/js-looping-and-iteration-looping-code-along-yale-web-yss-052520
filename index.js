4ce282242b7f6aa9453a71b484cb097da7aeff5e
Drew Beckmen <drew.beckmen@yale.edu>
2020-07-06 00:37:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, name) {
+    let return_arr = []; 
+    for (let i = 0; i < arr.length; i++) {
+        return_arr.push(`Thank you, ${arr[i]}, for the wonderful ${name} gift!`); 
+    }
+    return return_arr; 
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num); 
+        num--; 
+    }
+}
