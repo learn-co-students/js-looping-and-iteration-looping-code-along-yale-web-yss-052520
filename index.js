6f8ebf10690e6ce66e1949e42817efab14a2d77d
ev275 <enrique.valencia@yale.edu>
2020-07-07 00:04:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let cards = []
+    for (let i= 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(int) {
+    while ( int != -1 ) {
+        console.log(int--)
+    }
+}
