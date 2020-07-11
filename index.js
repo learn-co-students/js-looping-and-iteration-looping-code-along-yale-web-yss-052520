8a483650d57bfb88ad25f0d8b08b8c61c4ce4e2e
larissanguyen <larissa.h.nguyen@gmail.com>
2020-07-11 05:56:42 -0400

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 function writeCards(names, event) {
 	var messages = []
 	for (var i = 0; i < names.length; i++) {
-		messages.push(`Thank you, #{names[i]}, for the wonderful birthday gift!`)
+		messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+	}
+	return messages
+}
+
+function countDown(num) {
+	while (num >= 0) {
+		console.log(num)
+		num--
 	}
 }
